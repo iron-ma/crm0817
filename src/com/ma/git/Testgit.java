84435c601101125134a7bcfc9e47a33bf956c5a7
iron_ma <iron_ma@139.com>
2020-03-28 20:41:42 +0800

    local to remote

--- src/com/ma/git/Testgit.java
+++ src/com/ma/git/Testgit.java
@@ -26,6 +26,7 @@ public class Testgit {
         System.out.println("同步git服务器！！！01");
         System.out.println("同步git服务器！！！02");
         System.out.println("同步git服务器！！！03");
+        System.out.println("local 》》》 remote");
     }
 
 }
