12451e8cba7dc56bd57cb0184939d5f8a8c3ed3f
iron_ma <iron_ma@139.com>
2020-03-28 14:07:29 +0800

    create new Project

---
+++ src/com/ma/git/Testgit.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.ma.git;
+
+/**
+ * @Title:  Testgit.java
+ * @Description: 
+ * @author  ma_liang
+ * @date 2020年3月28日下午2:05:13
+ *
+ */
+public class Testgit {
+
+    /**
+     * @Title:  Testgit.java
+     * @Return: void
+     * @Description: 
+     * @author  ma_liang
+     * @date 2020年3月28日下午2:05:15
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("同步git服务器！！！");
+        
+
+    }
+
+}
